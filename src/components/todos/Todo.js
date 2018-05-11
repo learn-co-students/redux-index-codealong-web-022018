91bdbdabdbd1c53c81b0173054da8b913cc650d7
lidianvilazio <lidianvilazio@hotmail.com>
2018-05-11 09:43:54 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Todo extends Component {
-
   render() {
-    return(
-      <div>
-        Our Todo
-      </div>
-    );
-  }
+  return(
+    <div>
+      <form onSubmit={(event) => this.handleSubmit(event)}>
+        <p>
+          <label>add todo</label>
+          <input type="text" value={this.state.text} onChange={(event) => this.handleChange(event)} />
+        </p>
+        <input type="submit" />
+      </form>
+      /* code removed here */
+    </div>
+  );
+}
 };
 
 export default Todo;
